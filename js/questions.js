3f292d3ffe92870126cbb5272751859fbc611be2
Worthy Chukwuemeka <87069443+worthychukwuemeka@users.noreply.github.com>
2024-01-13 12:20:09 +0100

    Day 26, first commit

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which CSS property is used to change the text color of an element?",
+    answer: "color",
+    options: [
+      "color",
+      "text-color",
+      "font-color",
+      "textColor"
+    ]
+  },
+    {
+    numb: 3,
+    question: 'What is the purpose of the "document.getElementById()" function in JavaScript?',
+    answer: "To retrieve an HTML element based on its id",
+    options: [
+      "To get the value of an input field",
+      "To retrieve an HTML element based on its id",
+      "To create a new HTML element",
+      "To modify the CSS styles of an element"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the primary purpose of media queries in CSS for web development?",
+    answer: "To apply different styles based on the device characteristics",
+    options: [
+      "To create animations",
+      "To handle user interactions",
+      "To apply different styles based on the device characteristics",
+      "To define variables in CSS"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which version control system is widely used in web development for managing source code?",
+    answer: "Git",
+    options: [
+      "Git",
+      "SVN (Apache Subversion)",
+      "Mercurial",
+      "CVS (Concurrent Versions System)"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
